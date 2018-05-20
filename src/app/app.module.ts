e4a59446c9249371dab8031afe3d60a9ae6bc4dc
unknown <nagarjun.sanji@gmail.com>
2018-05-20 18:46:32 +0530

    Enhancement - updated package json - created header, footer, container and user-card using angular-cli command - used for loop to add more cards inside the container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './app/header/header.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
+import { UserCardComponent } from './container/user-card/user-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule
